30ddf7028bdd860122ee21b07e822436a7a2d8d2
Godwin Ogbonnaya Jnr <godwinogbonnayajnr@gmail.com>
2018-01-21 17:44:05 +0100

    Optimized using Chrome Coverage

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,4 @@
 
-
-// WOW
-$(function(){
-  
-  new WOW().init();
-
-});
-
 // Hide mobile menu after clicking on a link
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
